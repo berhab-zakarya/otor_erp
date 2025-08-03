0c696fdfffd10c4a1e0cbcc0e9d1f34eeb0990a4
berhab-zakarya <zakarya.berhab@univ-tlemcen.dz>
2025-08-03 15:04:49 +0100

    update

--- frontend/src/components/dashboard/staff/StaffControls.tsx
+++ frontend/src/components/dashboard/staff/StaffControls.tsx
@@ -7,7 +7,7 @@ function StaffControls() {
     return (
         <div className='mt-[40px] h-[150px] bg-white rounded-[10px] py-6 px-[20px] flex justify-between items-center'>
             <div className='flex flex-col gap-2'>
-                <h1 className='text-[#121212] text-sm'>Quick search a staff</h1>
+                <h1 className='text-[#121212] text-sm'>Quick search staff</h1>
 
                 <OtorSearchInput
                     width='w-[350px]'
@@ -36,7 +36,6 @@ function StaffControls() {
 
             <OtorButton
                 title="Add New Staff"
-
                 width='w-[180px]'
             />
         </div>
